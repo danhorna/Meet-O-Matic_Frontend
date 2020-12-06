2e156575315aab52319a452d9483440664e8b323
Dan Horna <dan_franco_15@hotmail.com>
2020-12-06 04:38:18 -0300

    login validator & redirect styles

--- src/app/components/routes/login/login.component.ts
+++ src/app/components/routes/login/login.component.ts
@@ -12,6 +12,9 @@ import { TokenserviceService } from 'src/app/services/tokenservice.service';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup
+  wrong: boolean = false
+  loading: boolean = false
+  logged: boolean = false
 
   constructor(
     private loginFormBuild: FormBuilder,
@@ -20,26 +23,52 @@ export class LoginComponent implements OnInit {
     private userController: UserControllerService
   ) {
     this.loginForm = this.loginFormBuild.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
+      email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
     })
   }
 
   ngOnInit(): void {
   }
 
-  login(){
-    if (this.loginForm.valid){
+  remo() {
+    if (this.wrong) {
+      this.wrong = false
+    }
+  }
+
+  delayRedirect() {
+    var count = 3;
+    var countdown = setInterval(() => {
+      $("#countdown").html("Redirigiendo en " + count);
+      if (count == 0) {
+        clearInterval(countdown);
+        this.activeRouter.navigateByUrl('/')
+      }
+      count--;
+    }, 1000);
+  }
+
+  login() {
+    if (this.loginForm.valid && !this.logged) {
+      this.loading = true
       const toSend = {
         email: this.loginForm.value.email,
         password: this.loginForm.value.password
       }
 
-      this.userController.userControllerLogin(toSend).subscribe((res)=>{
-        this.tokenService.saveToken(res.token)
-        this.activeRouter.navigateByUrl('/')
-      })
+      setTimeout(() => {
+        this.userController.userControllerLogin(toSend).subscribe((res) => {
+          this.loading = false
+          this.logged = true
+          this.tokenService.saveToken(res.token)
+          this.delayRedirect()
+        },
+          (err) => {
+            this.loading = false
+            this.wrong = true
+          })
+      }, 2000);
     }
   }
-
 }
