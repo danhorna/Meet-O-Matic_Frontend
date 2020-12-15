66f1eb0ea98472ee9454b2e7b8449f79f76754ed
Dan Horna <dan_franco_15@hotmail.com>
2020-12-15 20:50:17 -0300

    heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  TOKEN_KEY:'token',
+  USER_KEY:'user',
+  API_BASE_PATH: 'https://meet-o-matic-backend.herokuapp.com',
+  SITE_URL: 'https://meetomatic.herokuapp.com/',
+  EVENT_PATH: 'event/',
+  NAV_HIDE: ['premium', '404', 'event']
 };
